7ccf00208ce2ed66903e31e1ef2991a9da037297
tom.guan <tom.guan@sap.com>
2018-04-04 09:15:04 +0800

    init version

---
+++ review/src/main/java/com/tom/thread/App.java
@@ -0,0 +1,13 @@
+package com.tom.thread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
